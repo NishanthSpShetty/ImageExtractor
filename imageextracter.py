7fda32641c94bfc18d3ff3b3489ef7f3d00464d3
NishanthSpShetty <nishanthspshetty@gmail.com>
2016-07-13 22:34:21 +0530

    setting up base url for images

--- imageextracter.py
+++ imageextracter.py
@@ -4,6 +4,7 @@ import urllib2
 import os
 import sys
 from BeautifulSoup import BeautifulSoup
+from urlparse import urlparse
 
 IMG_BASE_DIR = './pictures/';
 
@@ -11,6 +12,12 @@ def imageLoader(URL,img_desc_file_name="img_desc_file.idf"):
     print "Loading web page : %s"%URL
     print "Please wait..."
     #fetch response
+
+    url_parsed = urlparse(URL)
+
+    domain = url_parsed.scheme+'://'+url_parsed.netloc
+
+    print domain
 
     opener = urllib2.build_opener()
     #spoof User-Agent
@@ -22,7 +29,8 @@ def imageLoader(URL,img_desc_file_name="img_desc_file.idf"):
     except Exception as e:
         print "Error [ %s ]"%e
         return
-
+    print "URL is",response.geturl()
+    print "Info ",response.info()
     soup = BeautifulSoup(response.read())
     print "Page loaded, searching for images in the response"
     imgs = soup.findAll("img",{"alt":True,"src":True}) #get src and title too
@@ -32,12 +40,20 @@ def imageLoader(URL,img_desc_file_name="img_desc_file.idf"):
     for img in imgs:
         #print img
         #print img["alt"]
-        img_url = img["src"];
+        img_url = img["src"].encode(sys.getfilesystemencoding());
         try :
+
+            if img_url.startswith('/'):
+                imgurl=domain+img_url
+            else :
+                imgurl=img_url
+
             #img_file = urllib2.urlopen(img_url)
-            img_file = opener.open(img_url)
+            img_file = opener.open(imgurl)
         except Exception as e:
             #ignore the error
+            print "skipped...",e
+
             continue
         img_file_name = IMG_BASE_DIR+img["alt"]+str(int(random.random()*10000))+'.'+ img_url.split('.')[-1]
         try:
@@ -45,10 +61,11 @@ def imageLoader(URL,img_desc_file_name="img_desc_file.idf"):
             data = "%02s\nDesc  : %s\nFilename : %s\n%s\n"%(count,img["alt"],img_file_name.encode(sys.getfilesystemencoding()),"-"*130)
             #print data
             img_desc_file.write(data)
-            with open(img_file_name.encode(sys.getfilesystemencoding()),"w") as f:
+            with open(img_file_name.encode(sys.getfilesystemencoding()),"wb") as f:
                 f.write(img_file.read())
             count+=1
-        except:
+        except Exception as e:
+            print "skipped...",e
             pass
     print count," images loaded into "+IMG_BASE_DIR
     img_desc_file.close();
@@ -63,7 +80,6 @@ def main():
         URL = sys.argv[1]
     else :
         URL = raw_input("Please enter the url ")
-    URL = "http://profilepicturedp.weebly.com/cute-alia-bhat-picture.html"
     imageLoader(URL)
 
 
